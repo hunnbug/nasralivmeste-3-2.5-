00c9cb3387e082f0d0378a9a92a7bc6834a4275f
Shaibmen <ibin2005636@gmail.com>
2024-04-28 23:05:02 +0300

    1.2

--- ribatskaya buhta/ribatskaya buhta.cpp
+++ ribatskaya buhta/ribatskaya buhta.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <random>
 #include <map>
+
 using namespace std;
 
 class Udochka
@@ -56,20 +57,23 @@ private:
 	int lvl;																			
 	int balance;
 public:
-	Ribak(string _name, int _lvl, int _balance)
-	{
-		name = _name;
-		lvl = _lvl;
-		balance = _balance;
-	}																					
+	Ribak() : name(""), lvl(0), balance(0) {} // Конструктор по умолчанию
+
+	Ribak(string _name, int _lvl, int _balance) : name(_name), lvl(_lvl), balance(_balance) {}
+	
 };	
 
 class StartGame
 {
+private:
+	Riba ribas[12];
+	Udochka udochki[6];
+	Ribak ribak;
+	string name;
+	int lvl;
+	int balance;
 public:
-	static void createRibki()
-	{
-		Riba ribas[12];
+	StartGame() { // Redacted by Funtik
 		ribas[0] = Riba("карась", (rand() % 500 + 100), (rand() % 500 + 10), 0, "common");
 		ribas[1] = Riba("сом", (rand() % 500 + 100), (rand() % 500 + 10), 0, "common");
 		ribas[2] = Riba("язь", (rand() % 500 + 100), (rand() % 500 + 10), 10, "uncommon");
@@ -82,10 +86,8 @@ public:
 		ribas[9] = Riba("Рома", (rand() % 2500 + 2000), (rand() % 2500 + 2000), 40, "legendary");
 		ribas[10] = Riba("Миша", (rand() % 2500 + 2000), (rand() % 2500 + 2000), 40, "legendary");
 		ribas[11] = Riba("здоровенный язь", (rand() % 2500 + 2000), (rand() % 2500 + 2000), 50, "legendary");
-	}
-
-	static void createUdochki()
-	{
+		// Не дай бог вы эту механику не сделаете или запорите твари.
+//-----------------------------------------------------------------------------------------------------------------------------------------------------
 		static Udochka udochki[6];
 		udochki[0] = Udochka("ловитель параши всякой", 150, 9);
 		udochki[1] = Udochka("перемотанная изолентой крепкая удочка", 250, 15);
@@ -93,32 +95,99 @@ public:
 		udochki[3] = Udochka("секретная удочка покрашенная розовойц краской", 450, 32);
 		udochki[4] = Udochka("цельнометаллическая удочка", 600, 41);
 		udochki[5] = Udochka("самая крутая удочка", 1000, 56);
+		// теперь даже можно к полям класса обратиться
+//-----------------------------------------------------------------------------------------------------------------------------------------------------
+		int lvl = 1;
+		int balance = 100;
+		cout << "введите имя рыбака: ";
+		cin >> name;
+		Ribak ribak(name, lvl, balance);
+		system("cls");
+
+
+	}
+	string getName() const {
+		return name;
 	}
 
-	static void createRibak()
-	{
-		string _name;
-		int _lvl = 1;
-		int _balance = 100;
+	int getLevel() const {
+		return lvl;
+	}
 
-		cout << "введите имя рыбака: ";
-		cin >> _name;
-
-		Ribak ribak(_name, _lvl, _balance);
-		cout << "добро пожаловать, " + _name + "!, ваш баланс: " + to_string(_balance) + ", ваш уровень: " + to_string(_lvl) << endl;
+	int getBalance() const {
+		return balance;
 	}
 };
 
-																						
-																						
+
+
+static void menu() {
+
+	StartGame game;
+
+	int choose;
+	cout << "|-------------------------------------------------------------------------------------------------|" << endl;
+	cout << "|Вы приходите в маленький рыбацкий городок, тут довольно много народу для обычного дня.           |" << endl;
+	cout << "|Люди на рынке суетятся и торговля идёт довольно оживлённо.                                       |" << endl; 
+	cout << "|-------------------------------------------------------------------------------------------------|" << endl;
+	cout << "|Куда отправитесь?" << endl;
+	cout << "|	1. Узнать характеристики" << endl;
+	cout << "|	2. Пойти в магазин" << endl;
+	cout << "|	3. Поррррррррррррррррррррыбачить?" << endl;
+	cout << "|	?. Прокачка статов" << endl; // <------- Думаем надо ли это? (можно сделать в магазе, но магаз будет мега нагружен, в консоли не удобно ориентироваться.
+	cout << "|	5. Выход" << endl;
+	cout << "|-------------------------------------------------------------------------------------------------|" << endl;
+	
+	cin >> choose;
+	while (true)
+	{
+		switch (choose)
+		{
+		case 1:
+			cout << "Имя: " << game.getName() << endl;
+			cout << "Уровень: " << game.getLevel() << endl;
+			cout << "Баланс: " << game.getBalance() << endl;
+			cout << " " << endl;
+			cout << "Для выхода введите 0" << endl;
+			cin >> choose;
+			if (choose == 0) {
+				menu();
+			}
+		case 2:
+
+		case 3:
+
+		case 4:
+
+		case 5:
+
+		default:
+			break;
+		}
+	}
+	
+}
+										
 int main()																		
-{																				
-																					
-	setlocale(LC_ALL, "RU");															
-	cout << "рыбацкая бухта" << endl;	
-	StartGame::createRibki();
-	StartGame::createUdochki();
-	StartGame::createRibak();
+{
+	setlocale(LC_ALL, "RU");
+
+	char trash;
+	cout << "От студии nasralivmeste-3-2.5-" << endl;
+	cout << "-------------------------------" << endl;
+	cout << "3 фрика энтертеймент" << endl;
+	cout << "-------------------------------" << endl;
+	cout << " " << endl;
+	cout << " " << endl;
+	cout << "Рыбатская бухта: новое начало." << endl;
+	cout << " " << endl;
+	cout << " " << endl;
+	cout << "-------------------------------" << endl;
+	cout << "Введите любой символ" << endl;
+	cin >> trash;
+	system("cls");
+	menu();
+	
 }			
 
 //короче это все полная хуета, я завтра переделаю эту залупу
